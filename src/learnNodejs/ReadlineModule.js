3ca22c7e69b8864fc49aa268105ce42e27e06aa9
Diana-nyamai <dnyamai.dn@gmail.com>
2021-08-30 17:27:33 +0300

    readline module

---
+++ src/learnNodejs/ReadlineModule.js
@@ -0,0 +1,32 @@
+const ReadLine = require('readline');
+const Rl = ReadLine.createInterface({input : process.stdin,
+                                    output : process.stdout});
+
+let num1 = Math.floor((Math.random() * 10 ) + 1);
+let num2 = Math.floor((Math.random() * 10) + 1);
+let answer = num1 + num2;
+
+Rl.question(`what is ${num1} + ${num2}? \n`, 
+(userInput) =>{
+     if(userInput.trim() == answer){
+         Rl.close();
+     }
+     else{
+         Rl.setPrompt('incorrect. try again \n');
+         Rl.prompt();
+         Rl.on('line', (userInput)=>{
+            if(userInput.trim() == answer){
+                Rl.close();
+            }
+            else{
+                Rl.setPrompt(`your answer of ${ userInput } is incorrect.try again \n`)
+                Rl.prompt();
+            }
+         })
+     }
+}
+);
+
+Rl.on('close', ()=>{
+    console.log('correct answer');
+})
